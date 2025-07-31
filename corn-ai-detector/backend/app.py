b0a42d16f3a3570e938423f5022f67e96645380e
michaeljames123 <147842705+michaeljames123@users.noreply.github.com>
2025-07-31 22:51:54 +0800

    Update app.py

--- corn-ai-detector/backend/app.py
+++ corn-ai-detector/backend/app.py
@@ -89,4 +89,7 @@ def result_image(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000)
+
+
+
